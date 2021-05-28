606d191ff15c787062aa2acfa6cb472260473465
Sanjay Srinivas <sanjay.srinivas@open.edu.au>
2021-05-28 19:19:58 +1000

    Debug

--- views/h.js
+++ views/h.js
@@ -6,7 +6,7 @@ exports.hView = {
         type: 'plain_text',
         text: 'Handoff Bot!'
     },
-    blocks: [
+    /*blocks: [
         {
             type: 'input',
             block_id: 'mobile_block_id',
@@ -18,6 +18,161 @@ exports.hView = {
                 type: 'plain_text_input',
                 action_id: 'mobile_input',
                 multiline: false
+            }
+        },
+        {
+            type: 'input',
+            block_id: 'mobile_block_id',
+            label: {
+                type: 'plain_text',
+                text: 'Mobile Number'
+            },
+            element: {
+                type: 'plain_text_input',
+                action_id: 'mobile_input',
+                multiline: false
+            }
+        },
+        {
+            type: 'input',
+            block_id: 'mobile_block_id',
+            label: {
+                type: 'plain_text',
+                text: 'Mobile Number'
+            },
+            element: {
+                type: 'plain_text_input',
+                action_id: 'mobile_input',
+                multiline: false
+            }
+        },
+        {
+            type: 'input',
+            block_id: 'mobile_block_id',
+            label: {
+                type: 'plain_text',
+                text: 'Mobile Number'
+            },
+            element: {
+                type: 'plain_text_input',
+                action_id: 'mobile_input',
+                multiline: false
+            }
+        }
+
+    ],*/
+    blocks: [
+        {
+            "type": "header",
+            "text": {
+                "type": "plain_text",
+                "text": "This is a header block",
+                "emoji": true
+            }
+        },
+        {
+            "type": "divider"
+        },
+        {
+            "type": "input",
+            "element": {
+                "type": "plain_text_input",
+                "action_id": "mobile-input-action"
+            },
+            "label": {
+                "type": "plain_text",
+                "text": "Mobile Number",
+                "emoji": true
+            }
+        },
+        {
+            "type": "input",
+            "element": {
+                "type": "plain_text_input",
+                "action_id": "email-input-action"
+            },
+            "label": {
+                "type": "plain_text",
+                "text": "Email",
+                "emoji": true
+            }
+        },
+        {
+            "type": "input",
+            "element": {
+                "type": "plain_text_input",
+                "action_id": "fname-input-action"
+            },
+            "label": {
+                "type": "plain_text",
+                "text": "First Name",
+                "emoji": true
+            }
+        },
+        {
+            "type": "input",
+            "element": {
+                "type": "plain_text_input",
+                "multiline": true,
+                "action_id": "notes-input-action"
+            },
+            "label": {
+                "type": "plain_text",
+                "text": "Notes",
+                "emoji": true
+            }
+        },
+        {
+            "type": "input",
+            "element": {
+                "type": "datepicker",
+                "initial_date": "1990-04-28",
+                "placeholder": {
+                    "type": "plain_text",
+                    "text": "Select a date",
+                    "emoji": true
+                },
+                "action_id": "datepicker-action"
+            },
+            "label": {
+                "type": "plain_text",
+                "text": "Date",
+                "emoji": true
+            }
+        },
+        {
+            "type": "input",
+            "element": {
+                "type": "timepicker",
+                "initial_time": "13:37",
+                "placeholder": {
+                    "type": "plain_text",
+                    "text": "Select time",
+                    "emoji": true
+                },
+                "action_id": "timepicker-action"
+            },
+            "label": {
+                "type": "plain_text",
+                "text": "Time",
+                "emoji": true
+            }
+        },
+        {
+            "type": "input",
+            "element": {
+                "type": "multi_users_select",
+                "placeholder": {
+                    "type": "plain_text",
+                    "text": "Select users",
+                    "emoji": true
+                },
+                "action_id": "users-select-action"
+            },
+            "label": {
+                "type": "plain_text",
+                "text": "Users",
+                "emoji": true
             }
         }
     ],
