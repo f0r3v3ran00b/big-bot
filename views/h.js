442605d17ca58f8c8af2dddce2be6f844054e150
Sanjay Srinivas <sanjay.srinivas@open.edu.au>
2021-05-28 19:06:10 +1000

    Fix

--- views/h.js
+++ views/h.js
@@ -4,35 +4,20 @@ exports.hView = {
     callback_id: 'hello_view',
     title: {
         type: 'plain_text',
-        text: 'Modal title'
+        text: 'Handoff Bot!'
     },
     blocks: [
-        /*{
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: 'Welcome to a modal with _blocks_'
-            },
-            accessory: {
-                type: 'button',
-                text: {
-                    type: 'plain_text',
-                    text: 'Click me!'
-                },
-                action_id: 'button_abc'
-            }
-        },*/
         {
             type: 'input',
-            block_id: 'input_c',
+            block_id: 'mobile_block_id',
             label: {
                 type: 'plain_text',
-                text: 'What are your hopes and dreams?'
+                text: 'Mobile Number'
             },
             element: {
                 type: 'plain_text_input',
-                action_id: 'dreamy_input',
-                multiline: true
+                action_id: 'mobile_input',
+                multiline: false
             }
         }
     ],
